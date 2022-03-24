cc7997659ae15537e68c040c50254b097bd839db
Eder Costa <eder@b2mlportal.com.br>
2022-03-24 17:10:02 -0300

    Ajustes projeto

--- dart_funcoes_colecoes/bin/dart_funcoes_colecoes.dart
+++
@@ -1,5 +0,0 @@
-import 'package:dart_funcoes_colecoes/dart_funcoes_colecoes.dart' as dart_funcoes_colecoes;
-
-void main(List<String> arguments) {
-  print('Hello world: ${dart_funcoes_colecoes.calculate()}!');
-}
--- dart_funcoes_colecoes/test/dart_funcoes_colecoes_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dart_funcoes_colecoes/dart_funcoes_colecoes.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
