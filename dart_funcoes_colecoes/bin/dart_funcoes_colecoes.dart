09545a18a2c201eeaf3b6abc255c984c35fcb800
Eder Costa <eder@b2mlportal.com.br>
2022-03-24 17:02:24 -0300

    2 modulo

---
+++ dart_funcoes_colecoes/bin/dart_funcoes_colecoes.dart
@@ -0,0 +1,5 @@
+import 'package:dart_funcoes_colecoes/dart_funcoes_colecoes.dart' as dart_funcoes_colecoes;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_funcoes_colecoes.calculate()}!');
+}
---
+++ dart_funcoes_colecoes/test/dart_funcoes_colecoes_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_funcoes_colecoes/dart_funcoes_colecoes.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
