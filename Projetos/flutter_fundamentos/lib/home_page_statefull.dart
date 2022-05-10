313be7d96ba7440929a67b7d307c675f300f1db8
Eder Costa <eder@b2mlportal.com.br>
2022-05-10 17:35:04 -0300

    Executando funções depois da tela pronta (addPostFrameCallback)

--- Projetos/flutter_fundamentos/lib/home_page_statefull.dart
+++ Projetos/flutter_fundamentos/lib/home_page_statefull.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_fundamentos/home_page.dart';
 
 class HomePageStateFull extends StatefulWidget {
   @override
@@ -15,24 +16,34 @@ class _HomePageStateFull extends State<HomePageStateFull> {
   void initState() {
     super.initState();
     texto = 'Texto alterado pelo initState';
+
+    WidgetsBinding.instance?.addPostFrameCallback((timeStamp) {
+      print('depois da tela ter sido carregada');
+
+      Navigator.of(context).push(MaterialPageRoute(
+        builder: (context) => HomePageStateless(),
+      ));
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Column(
-        children: [
-          Text(texto),
-          TextButton(
-            onPressed: () {
-              setState(() {
-                texto = 'Alterado o texto via button stateFullWidget';
-              });
-            },
-            child: const Text('Alterar Texto'),
-          )
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(texto),
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  texto = 'Alterado o texto via button stateFullWidget';
+                });
+              },
+              child: const Text('Alterar Texto'),
+            )
+          ],
+        ),
       ),
     );
   }
