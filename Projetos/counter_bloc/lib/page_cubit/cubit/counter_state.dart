df9ff16cfd49747f398319dd8944e409752db9dd
Eder Costa <eder@b2mlportal.com.br>
2022-07-07 09:51:15 -0300

    Equatable

--- Projetos/counter_bloc/lib/page_cubit/cubit/counter_state.dart
+++ Projetos/counter_bloc/lib/page_cubit/cubit/counter_state.dart
@@ -1,14 +1,16 @@
 part of 'counter_cubit.dart';
 
-abstract class CounterState {
+abstract class CounterState extends Equatable {
   final int counter;
   const CounterState(this.counter);
+  @override
+  List<Object> get props => [counter];
 }
 
 class CounterStateInitial extends CounterState {
-  CounterStateInitial() : super(0);
+  const CounterStateInitial() : super(0);
 }
 
 class CounterStateData extends CounterState {
-  CounterStateData(int counter) : super(counter);
+  const CounterStateData(int counter) : super(counter);
 }
