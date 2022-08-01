22f31aca0b1cb3329fca0491a781ee235dbd39c5
Eder Costa <eder@b2mlportal.com.br>
2022-08-01 14:04:11 -0300

    construtor tear_off

---
+++ dart_poo/lib/22_construtor_tear_off/1_construtor_tear_off.dart
@@ -0,0 +1,19 @@
+void main(List<String> args) {
+  final nomes = ['Eder', 'Rodrigo', 'Renato', 'Sandra'];
+  final pessoas_old = nomes.map((e) => Pessoa(e)).toList();
+
+  // ignore: sdk_version_constructor_tearoffs
+  // final pessoas = nomes.map<Pessoa>(Pessoa.nome).toList();
+    final pessoas = nomes.map<Pessoa>(Pessoa.new).toList();
+
+  for (var pessoa in pessoas) {
+    print('Olá ${pessoa.nome}');
+  }
+}
+
+class Pessoa {
+  String nome;
+  Pessoa(this.nome);
+
+  Pessoa.nome(this.nome);
+}
