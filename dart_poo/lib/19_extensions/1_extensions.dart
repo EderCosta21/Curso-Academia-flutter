18a58d7e850be82380999377b6b15ac8eecc1fe7
Eder Costa <eder@b2mlportal.com.br>
2022-08-01 10:24:45 -0300

    extension -> adicionar funcoes a codigo fonte

---
+++ dart_poo/lib/19_extensions/1_extensions.dart
@@ -0,0 +1,7 @@
+import './saudacao_string_extention.dart';
+
+void main(List<String> args) {
+  String nome = ' Eder';
+
+  print(nome.saudacao());
+}
---
+++ dart_poo/lib/19_extensions/saudacao_string_extention.dart
@@ -0,0 +1,5 @@
+extension SaudacaoString on String {
+  String saudacao() {
+    return 'ola $this welcome to Academia do Flutter';
+  }
+}
